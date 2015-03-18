4934f4ed0bd67423495db6c9f9904a4cfaebb345
blackbelt <emanuele.g@gmail.com>
2015-03-18 23:46:09 +0100

    quicksort and heapsort

--- src/com/blackbelt/utils/Utils.java
+++ src/com/blackbelt/utils/Utils.java
@@ -1,5 +1,7 @@
 package com.blackbelt.utils;
 
+import java.util.Arrays;
+
 /**
  * Created by emanuele on 15.03.15.
  */
@@ -45,11 +47,101 @@ public class Utils {
         if (n == 1) {
             return a;
         }
-
         if ((n & 1) == 0) {
             return pow(a * a, n >> 1);
         } else {
             return a * pow(a * a, (n - 1) >> 1);
         }
+    }
+
+    private static void swap(int[] array, int firstIndex, int endIndex) {
+        int tmp = array[firstIndex];
+        array[firstIndex] = array[endIndex];
+        array[endIndex] = tmp;
+    }
+
+    private static int partition(int[] array, int start, int end) {
+        int pivotIndex = start + (end - start) / 2;
+        int pivot = array[pivotIndex];
+        swap(array, pivotIndex, end);
+        int cutIndex = start - 1;
+        for (int i = start; i < end; i++) {
+            if (array[i] < pivot) {
+                swap(array, ++cutIndex, i);
+            }
+        }
+        swap(array, ++cutIndex, end);
+        return cutIndex;
+    }
+
+    private static void simpleQuickSort(int[] a, int lo, int hi) {
+        if (lo > hi) {
+            return;
+        }
+        int cutIndex = partition(a, lo, hi);
+        simpleQuickSort(a, lo, cutIndex - 1);
+        simpleQuickSort(a, cutIndex + 1, hi);
+    }
+
+    public static void sort(int[] a) {
+       heapSort(a);
+       // simpleQuickSort(a, 0, a.length - 1);
+        assert isSorted(a, 0, a.length - 1);
+    }
+
+    private static void heapify(int[] a, int size, int index) {
+        int leftIndex = (index << 1) + 1;
+        int rightIndex = (leftIndex) + 1;
+        int largestIndex = index;
+        if (leftIndex < size && a[leftIndex] > a[largestIndex]) {
+            largestIndex = leftIndex;
+        }
+        if (rightIndex < size && a[rightIndex] > a[largestIndex]) {
+            largestIndex = rightIndex;
+        }
+        if (largestIndex != index) {
+            swap(a, largestIndex, index);
+            heapify(a, size, largestIndex);
+        }
+    }
+
+    private static void heapSort(int[] a) {
+        buildHeap(a);
+        int lastSwapIndex = a.length - 1;
+        for (int i = a.length - 1; i >= 0; i--) {
+            swap(a, 0, lastSwapIndex);
+            heapify(a, lastSwapIndex, 0);
+            lastSwapIndex--;
+        }
+    }
+
+    private static void buildHeap(int[] a) {
+        for (int i = a.length / 2 - 1; i >= 0; i--) {
+            heapify(a, a.length, i);
+        }
+    }
+
+    private static void mergeSort(int[] a) {
+        int[] tmp = new int[a.length];
+    }
+
+    private static boolean isSorted(int[] a, int lo, int hi) {
+        for (int i = lo; i < hi - 1; i++)
+            if (isGreater(a[i], a[i + 1])) {
+                return false;
+            }
+        return true;
+    }
+
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo; i < hi - 1; i++)
+            if (isGreater(a[i], a[i + 1])) {
+                return false;
+            }
+        return true;
+    }
+
+    private static boolean isGreater(Comparable first, Comparable second) {
+        return first.compareTo(second) > 0;
     }
 }
